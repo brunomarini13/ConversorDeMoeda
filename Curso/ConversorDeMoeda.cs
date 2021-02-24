269d2dc7423d9cbc7462f8e9e78cc4ff3ad354ae
Bruno Marini <brunook13@outlook.com>
2021-02-23 21:44:11 -0300

    Add project files.

---
+++ Curso/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Curso
+{
+    class ConversorDeMoeda
+    {
+        public static double Cotacao;
+        public static double Dolares;
+        public static double Conversao()
+        {
+            return Cotacao * Dolares + 0.06 * Cotacao * Dolares;
+        }
+
+    }
+}
